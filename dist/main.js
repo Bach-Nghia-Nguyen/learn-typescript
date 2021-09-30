a6befd294711fa1f06c2fb620c2285b5df45aa52
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2021-09-30 11:37:00 +0700

    any, void, never, unknown and type assertion

--- dist/main.js
+++ dist/main.js
@@ -22,7 +22,28 @@ var dragonTag = "dragon";
 // Union operator
 var username = "Alex";
 var pageName = "1";
-console.log(typeof pageName);
+// console.log(typeof pageName);
 var errorMessage = null;
 var user3 = null;
 var someProp;
+var doSomething = function () {
+    console.log("Do something");
+};
+var foo = "meet";
+foo = 7;
+// console.log(foo.bar());
+var endless = function () {
+    throw "Neved";
+};
+// console.log(endless());
+var vAny = 10;
+var vUnknown = 10;
+var s1 = vAny;
+// console.log(typeof s1);
+var s2 = vUnknown;
+// console.log(typeof s2);
+// console.log(vAny.foo());
+// console.log(vUnknown.foo());
+var pageNumber = "1";
+var numericPageNumber = pageNumber;
+console.log(typeof numericPageNumber);
--- src/main.ts
+++ src/main.ts
@@ -47,9 +47,38 @@ const dragonTag: PossiblePopularTag = "dragon";
 // Union operator
 let username: string = "Alex";
 let pageName: string | number = "1";
-console.log(typeof pageName);
+// console.log(typeof pageName);
 let errorMessage: string | null = null;
 
 let user3: UserInterFace | null = null;
 
 let someProp: string | number | null | undefined | string[] | object;
+
+const doSomething = (): void => {
+  console.log("Do something");
+};
+
+let foo: any = "meet";
+foo = 7;
+// console.log(foo.bar());
+
+const endless = (): never => {
+  throw "Neved";
+};
+
+// console.log(endless());
+
+let vAny: any = 10;
+let vUnknown: unknown = 10;
+
+let s1: string = vAny;
+// console.log(typeof s1);
+let s2: string = vUnknown as string;
+// console.log(typeof s2);
+
+// console.log(vAny.foo());
+// console.log(vUnknown.foo());
+
+let pageNumber: string = "1";
+let numericPageNumber: number = pageNumber as unknown as number;
+console.log(typeof numericPageNumber);
